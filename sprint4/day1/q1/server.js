5000ba099a258dc126fcbf8614db66b0e07ec0b8
Everus <everuslainus@gmail.com>
2024-09-28 15:45:03 +0530

    video streaming

--- sprint4/day1/q1/server.js
+++ sprint4/day1/q1/server.js
@@ -43,6 +43,6 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-app.listen(3000, () => {
-  console.log("Server listening on http://localhost:3000");
+app.listen(4000, () => {
+  console.log("Server listening on http://localhost:4000");
 });
